6659a2b801091c72ab53808d641f385ab05ebcd1
Scott Matsuda <smatsuda2014@gmail.com>
2022-09-12 11:55:27 -0700

    Completed BeesBeesBees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.apply(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.canFly = true;
+  this.treasureChest = [];
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
 };
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.apply(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = 'yellow';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  return this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  return this.honeyPot--;
+};
